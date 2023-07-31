307d9026a999913fe9acbc138fed60957e024a54
Raine <64136899+raineio@users.noreply.github.com>
2023-08-01 00:42:06 +0200

    redo project

---
+++ main.ts
@@ -0,0 +1,29 @@
+import express from "express";
+import { setupRoutes } from "./router";
+import { Server } from "socket.io";
+
+async function main() {
+    const httpPort = 80; // Private port, public secure port is 443, which gets routed through nginx.
+    const socketPort = 8080;
+    const app = express();
+    const socketServer = new Server(socketPort);
+    
+    console.log(`Socket server started on port ${socketPort}`);
+    
+    setupRoutes(app);
+
+    app.use(express.json());
+    app.use(express.static("public"));
+    
+    app.listen(httpPort, () => {
+        console.log(`App is listening to port ${httpPort}!`)
+
+    });
+
+    socketServer.on("connection", (socket) => {
+        console.log(`New listener connected.\nID: ${socket.id}\nIP: ${socket.handshake.address} (${socket.handshake.headers['x-forwarded-for']})`);
+    });
+
+}
+
+main();
