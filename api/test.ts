eed8f07e091a31c6c1594349d2f385409cc1ee8b
Raine <64136899+raineio@users.noreply.github.com>
2023-08-01 20:16:30 +0200

    this is how its done from now on

--- api/test.ts
+++ api/test.ts
@@ -1,7 +1,9 @@
 import { Response, Request } from "express";
 import { GET } from "router";
 
-@GET("/test")
-function Test(req: Request, res: Response) {
-    res.send("Test");
+export class Test {
+    @GET("/")
+    getTest(req: Request, res: Response) {
+        res.send("Hello, World!")
+    }
 }
