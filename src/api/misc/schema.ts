45624d20f9d4b337ae02c8324f99641668910077
Raine <64136899+raineio@users.noreply.github.com>
2023-08-03 03:24:37 +0200

    fixed it

---
+++ src/api/misc/schema.ts
@@ -0,0 +1,9 @@
+import { Response, Request } from "express";
+import { GET } from "../../router";
+
+export class Schema {
+    @GET("schema/swagger")
+    getSwagger(res: Response, req: Request) {
+        res.status(200).send("swagger coming soon")
+    }
+}
