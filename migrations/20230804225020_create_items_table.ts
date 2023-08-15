93fcf9c12918fea0b34f1cbb45a6ebfb3d4fe0c6
Raine <64136899+raineio@users.noreply.github.com>
2023-08-15 18:32:50 +0200

    eslint is angy

--- migrations/20230804225020_create_items_table.ts
+++ migrations/20230804225020_create_items_table.ts
@@ -3,7 +3,7 @@ import { Knex } from "knex";
 export async function up(knex: Knex): Promise<void> {
     await knex.schema.createTable("items", (table) => {
         table.uuid("id").defaultTo(knex.raw("gen_random_uuid()")).primary();
-        table.string("name_id", 10)
+        table.string("name_id", 10);
         table.string("image", 70); //Most URLs have ~50 characters so we should pad it a bit to be safe.
         table.string("name", 25);
         table.integer("value");
