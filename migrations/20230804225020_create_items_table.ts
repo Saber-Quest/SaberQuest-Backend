f5bda0c9fd7708805d7523b3060efbf725c27f75
StormPacer <nejc.zivic@gmail.com>
2023-09-19 20:39:06 +0200

    We love forgetting to drop the db

--- migrations/20230804225020_create_items_table.ts
+++ migrations/20230804225020_create_items_table.ts
@@ -31,7 +31,7 @@ export async function up(knex: Knex): Promise<void> {
             .references("name_id")
             .inTable("items")
             .onDelete("CASCADE");
-    })
+    });
 }
 
 export async function down(knex: Knex): Promise<void> {
