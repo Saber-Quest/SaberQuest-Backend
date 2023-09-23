ac33316e7ba960530d38bef386d8bdd577d71210
StormPacer <nejc.zivic@gmail.com>
2023-09-23 19:00:34 +0200

    Update functions done + documentation

--- src/api/users/update.ts
+++ src/api/users/update.ts
@@ -328,52 +328,178 @@ export class Update {
         }
     }
 
+    /**
+     * Update banner
+     * @typedef {object} Banner
+     * @property {string} banner.required - The base64 encoded banner image
+     * @property {string} type.required - The banner type
+     * @property {string} token.required - The user's JWT token
+     */
+
+    /**
+     * PUT /update/banner
+     * @summary Update the user's banner
+     * @tags Update
+     * @security JWT
+     * @param {Banner} request.body.required - The base64 encoded banner image
+     * @return {string} 200 - Success
+     * @return {object} 400 - Missing fields
+     * @return {object} 400 - Invalid type
+     * @return {object} 400 - Not a patreon
+     * @return {object} 400 - Invalid token
+     * @return {string} 500 - Internal server error
+     * @example response - 200 - Success
+     * "OK"
+     * @example response - 400 - Missing fields
+     * {
+     * "error": "Missing fields"
+     * }
+     * @example response - 400 - Invalid type
+     * {
+     * "error": "Invalid type"
+     * }
+     * @example response - 400 - Not a patreon
+     * {
+     * "error": "Not a patreon"
+     * }
+     * @example response - 400 - Invalid token
+     * {
+     * "error": "Invalid token"
+     * }
+     * @example response - 500 - Internal server error
+     * "Internal server error"
+     */
+
     @PUT("update/banner")
     async banner(req: Request, res: Response) {
-        const { banner, token } = req.body;
-        const type = req.query.type;
+        try {
+            const { banner, token, type } = req.body;
 
-        if (!banner || !token || !type) {
-            return res.status(400).json({ error: "Missing fields" });
-        }
-
-        if (type !== "hor" && type !== "ver") {
-            return res.status(400).json({ error: "Invalid type" });
-        }
-
-        const decoded = verifyJWT(token);
-
-        if (!decoded || decoded.exp < Date.now() / 1000) {
-            return res.status(400).json({ error: "Invalid token" });
-        }
-
-        const user = await db<User>("users")
-            .select("patreon")
-            .where("platform_id", decoded.id)
-            .first();
-
-        if (!user.patreon) {
-            return res.status(400).json({ error: "Not a patreon" });
-        }
-
-        const buffer = Buffer.from(banner, "base64");
-
-        const dimensions = sizeOf(buffer);
-
-        if (type === "hor") {
-            if (dimensions.width !== 750 || dimensions.height !== 150) {
-                return res.status(400).json({ error: "Invalid banner" });
+            if (!banner || !token || !type) {
+                return res.status(400).json({ error: "Missing fields" });
             }
-        } else {
-            if (dimensions.width !== 425 || dimensions.height !== 820) {
-                return res.status(400).json({ error: "Invalid banner" });
+
+            if (type !== "hor" && type !== "ver") {
+                return res.status(400).json({ error: "Invalid type" });
             }
+
+            const decoded = verifyJWT(token);
+
+            if (!decoded || decoded.exp < Date.now() / 1000) {
+                return res.status(400).json({ error: "Invalid token" });
+            }
+
+            const user = await db<User>("users")
+                .select("patreon")
+                .where("platform_id", decoded.id)
+                .first();
+
+            if (!user.patreon) {
+                return res.status(400).json({ error: "Not a patreon" });
+            }
+
+            const buffer = Buffer.from(banner, "base64");
+
+            const dimensions = sizeOf(buffer);
+
+            if (type === "hor") {
+                if (dimensions.width !== 750 || dimensions.height !== 150) {
+                    return res.status(400).json({ error: "Invalid banner" });
+                }
+            } else {
+                if (dimensions.width !== 425 || dimensions.height !== 820) {
+                    return res.status(400).json({ error: "Invalid banner" });
+                }
+            }
+
+            downloadBanner(buffer, decoded.id, type);
+
+            await db<User>("users")
+                .update("banner", true)
+                .where("platform_id", decoded.id);
+        } catch (err) {
+            console.error(err);
+            return res.sendStatus(500);
         }
+    }
 
-        downloadBanner(buffer, decoded.id, type);
+    /**
+     * Update preference
+     * @typedef {object} Preference
+     * @property {string} preference.required - The preference
+     * @property {string} token.required - The user's JWT token
+     */
 
-        await db<User>("users")
-            .update("banner", true)
-            .where("platform_id", decoded.id);
+    /**
+     * PUT /update/preference
+     * @summary Update the user's preference
+     * @tags Update
+     * @security JWT
+     * @param {Preference} request.body.required - The preference
+     * @return {string} 200 - Success
+     * @return {object} 400 - Missing fields
+     * @return {object} 400 - Invalid preference
+     * @return {object} 400 - Invalid token
+     * @return {string} 500 - Internal server error
+     * @example response - 200 - Success
+     * "OK"
+     * @example response - 400 - Missing fields
+     * {
+     * "error": "Missing fields"
+     * }
+     * @example response - 400 - Invalid preference
+     * {
+     * "error": "Invalid preference"
+     * }
+     * @example response - 400 - Invalid token
+     * {
+     * "error": "Invalid token"
+     * }
+     * @example response - 500 - Internal server error
+     * "Internal server error"
+     */
+
+    @PUT("update/preference")
+    async preference(req: Request, res: Response) {
+        try {
+            const { preference, token } = req.body;
+
+            if (!preference || !token) {
+                return res.status(400).json({ error: "Missing fields" });
+            }
+
+            if (preference !== "ss" && preference !== "bl") {
+                return res.status(400).json({ error: "Invalid preference" });
+            }
+
+            const decoded = verifyJWT(token);
+
+            if (!decoded || decoded.exp < Date.now() / 1000) {
+                return res.status(400).json({ error: "Invalid token" });
+            }
+
+            if (decoded.id.startsWith("765") && decoded.id.length > 10 && preference === "ss") {
+                await db<User>("users")
+                    .update("preference", "ss")
+                    .where("platform_id", decoded.id);
+            }
+
+            else if (preference === "bl") {
+                await db<User>("users")
+                    .update("preference", "bl")
+                    .where("platform_id", decoded.id);
+            }
+
+            else {
+                return res.status(400).json({ error: "Invalid preference" });
+            }
+
+            clearUserCache(decoded.id);
+
+            res.sendStatus(200);
+        } catch (err) {
+            console.error(err);
+            return res.sendStatus(500);
+        }
     }
 }
