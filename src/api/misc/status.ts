d0f33a6da8688cfe9a81b8a24d290fb47a1f9952
StormPacer <nejc.zivic@gmail.com>
2023-09-29 18:47:20 +0200

    Documentation

--- src/api/misc/status.ts
+++ src/api/misc/status.ts
@@ -2,30 +2,47 @@ import { GET } from "../../router";
 import { Request, Response } from "express";
 
 export class Status {
+    /**
+     * GET /status
+     * @summary Get the status of the API
+     * @tags Misc
+     * @return {object} 200 - Success
+     * @return {string} 500 - Internal server error
+     * @example response - 200 - Success
+     * {
+     * "uptime": "0d 0h 0m 0s",
+     * "uptimeSeconds": 0,
+     * "cpuUsage": 0,
+     * "memUsage": 0
+     * }
+     * @example response - 500 - Internal server error
+     * "Internal server error"
+     */
     @GET("status")
     async get(req: Request, res: Response) {
-        const uptime = process.uptime();
-        const days = Math.floor(uptime / 86400);
-        const hours = Math.floor(uptime / 3600) % 24;
-        const minutes = Math.floor(uptime / 60) % 60;
-        const seconds = Math.floor(uptime % 60);
+        try {
+            const uptime = process.uptime();
+            const days = Math.floor(uptime / 86400);
+            const hours = Math.floor(uptime / 3600) % 24;
+            const minutes = Math.floor(uptime / 60) % 60;
+            const seconds = Math.floor(uptime % 60);
 
-        const memUsage = process.memoryUsage();
+            const memUsage = process.memoryUsage();
+            const cpuUsage = process.cpuUsage();
 
-        // Get the CPU usage in percentage since the last call
-        const cpuUsage = process.cpuUsage();
+            const cpuUsagePercent = Math.round((cpuUsage.user + cpuUsage.system) / 1000 / 1000 / uptime);
 
-        const cpuUsagePercent = Math.round(
-            (cpuUsage.user + cpuUsage.system) / 1000 / 1000 / uptime
-        );
+            const JsonResponse = {
+                uptime: `${days}d ${hours}h ${minutes}m ${seconds}s`,
+                uptimeSeconds: uptime,
+                cpuUsage: cpuUsagePercent,
+                memUsage: parseFloat((memUsage.heapUsed / 1000000).toFixed(2)),
+            };
 
-        const JsonResponse = {
-            uptime: `${days}d ${hours}h ${minutes}m ${seconds}s`,
-            uptimeSeconds: uptime,
-            cpuUsage: cpuUsagePercent,
-            memUsage: parseFloat((memUsage.heapUsed / 1000000).toFixed(2)),
-        };
-
-        return res.status(200).json(JsonResponse);
+            return res.status(200).json(JsonResponse);
+        } catch (err) {
+            console.error(err);
+            return res.sendStatus(500);
+        }
     }
 }
